26b20f3f2f01b44cfb8e886e4519ab4cda92278a
unknown <jasminesaimarantha@gmail.com>
2023-10-22 04:01:47 +0700

    fix: image doesnt change

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-const images = [
-    'assets/StarRail_Image_1696141531.png',
-    'assets/StarRail_Image_1697858717.png',
-    'assets/StarRail_Image_1697858971.png',
-];
-
-const text = [
-    'This is the Trailblazer.',
-    'This is Lynx.',
-    'This is Sushang.',
+const content = [
+    {
+        image: 'assets/StarRail_Image_1696141531.png',
+        text: 'This is the Trailblazer.',
+    },
+    {
+        image: 'assets/StarRail_Image_1697858717.png',
+        text: 'This is Lynx.',
+    },
+    {
+        image: 'assets/StarRail_Image_1697858971.png',
+        text: 'This is Sushang.',
+    },
 ];
 
 let currentIndex = 0;
 
 function changeContent(direction) {
     if (direction === 'prev') {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        currentIndex = (currentIndex - 1 + content.length) % content.length;
     } else if (direction === 'next') {
-        currentIndex = (currentIndex + 1) % images.length;
+        currentIndex = (currentIndex + 1) % content.length;
     }
 
     const changeableImg = document.getElementById('changeable_img');
     const changeableText = document.getElementById('changeable_text');
 
-    changeableImg.src = images[currentIndex];
-    changeableText.textContent = text[currentIndex];
+    changeableImg.src = content[currentIndex].image;
+    changeableText.textContent = content[currentIndex].text;
 }
